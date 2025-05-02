73c99c8036e0896a88c176ad6ecb9748619acc14
shivakrishna28 <145094879+shivakrishna28@users.noreply.github.com>
2025-05-02 09:37:35 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-gray-800 text-white py-4 mt-10">
+    <div className="container mx-auto text-center">
+      &copy; 2025 MyShop. All rights reserved.
+    </div>
+  </footer>
+);
+
+export default Footer;
